8ce783e8983fb09eba2e2a25393a7fc7d8328b3d
A01252512 <a01252512@itesm.mx>
2021-09-10 16:50:26 +0000

    ExamenParcial_P2

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,17 @@
-
+def cuadrado_mayor(n):
+    i = 0
+    mayor = False
+    while mayor == False:
+        if i**2 > n:
+            return i
+            mayor == True
+        else:
+            i += 1
 
 def main():
     numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-
+    print(cuadrado_mayor(numero))
 
 if __name__=='__main__':
     main()
